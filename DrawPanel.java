ad92efefb681f2c8212ddcd11052788fdecb945c
Nimoha12 <nimoa.haji@gmail.com>
2026-02-20 12:42:41 +0100

    Enhance DrawPanel for dynamic car and workshop rendering
    Refactor DrawPanel to support multiple car and workshop images.

--- DrawPanel.java
+++ DrawPanel.java
@@ -4,53 +4,67 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.System.out;
 
 // This panel represents the animated part of the view with the car images.
 
 public class DrawPanel extends JPanel{
+    //list of cars from CarController
+    ArrayList<Car> cars;
+    ArrayList<Workshop<Volvo240>> workshops;
+    //Map that matches cars to respective car image
+    Map<Class<? extends Movable>, BufferedImage> images = new HashMap<>();
 
-    // Just a single image, TODO: Generalize
-    BufferedImage volvoImage;
-    // To keep track of a single car's position
-    Point volvoPoint = new Point();
-
-    BufferedImage volvoWorkshopImage;
-    Point volvoWorkshopPoint = new Point(300,300);
-
-    // TODO: Make this general for all cars
-    void moveit(int x, int y){
-        volvoPoint.x = x;
-        volvoPoint.y = y;
-    }
+    //Map for workshop pictures
+    Map<Class<? extends Workshop<?>>, BufferedImage> WorkshopImages = new HashMap<>();
+    //Map for workshop Points
+    Map<Class<? extends Workshop<?>>, Point> workshopPositions = new HashMap<>();
 
     // Initializes the panel and reads the images
-    public DrawPanel(int x, int y) {
+    public DrawPanel(int x, int y, ArrayList<Car> cars, ArrayList<Workshop<Volvo240>> workshops) {
+        this.cars = cars;
+        this.workshops = workshops;
         this.setDoubleBuffered(true);
         this.setPreferredSize(new Dimension(x, y));
-        this.setBackground(Color.green);
+        this.setBackground(Color.lightGray);
+
         // Print an error message in case file is not found with a try/catch block
         try {
-            // You can remove the "pics" part if running outside of IntelliJ and
-            // everything is in the same main folder.
-            // volvoImage = ImageIO.read(new File("Volvo240.jpg"));
+            //Add car images
+            images.put(Volvo240.class, ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg")));
+            images.put(Saab95.class,   ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Saab95.jpg")));
+            images.put(Scania.class,   ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Scania.jpg")));
 
-            // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
-            // if you are starting in IntelliJ.
-            volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg"));
-            volvoWorkshopImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg"));
+            //Add workshop images
+            WorkshopImages.put(VolvoWorkshop.class, ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg")));
+
+            //Add workshop points
+            workshopPositions.put(VolvoWorkshop.class, new Point(300,0));
         } catch (IOException ex)
         {
             ex.printStackTrace();
         }
-
     }
 
     // This method is called each time the panel updates/refreshes/repaints itself
-    // TODO: Change to suit your needs.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(volvoImage, volvoPoint.x, volvoPoint.y, null); // see javadoc for more info on the parameters
-        g.drawImage(volvoWorkshopImage, volvoWorkshopPoint.x, volvoWorkshopPoint.y, null);
-    }
-}
+        for (Car car : cars){
+            BufferedImage image = images.get(car.getClass());
+            int x = (int) car.getX();
+            int y = (int) car.getY();
+            g.drawImage(image, x, y, null);
+        }
+
+        for (Workshop<?> ws : workshops){
+            BufferedImage img = WorkshopImages.get(ws.getClass());
+            Point pos = workshopPositions.get(ws.getClass());
+            if(img !=null && pos !=null){
+            g.drawImage(img, pos.x, pos.y, null);
+        }}
+    }}
